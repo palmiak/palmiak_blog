4bd1178ed43c281453f2fd37bf85d59401113fe3
Agnieszka Palmowska <a.palmowska@freshpixels.pl>
2022-04-21 23:56:27 +0200

    css fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,23 @@
 module.exports = {
-  content: [
-    './resources/**/*.antlers.html',
-    './resources/**/*.blade.php',
-    './resources/**/*.vue',
-    './content/**/*.md'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+    content: [
+        "./resources/**/*.antlers.html",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.vue",
+        "./content/**/*.md",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                "palmiak-font": ['"Plus Jakarta Sans"', "sans-serif"],
+                "palmiak-font-serif": ['"Martel"', "serif"],
+            },
+
+            colors: {
+                palmiak_pink: "#FA8BFF",
+                palmiak_blue: "#2BD2FF",
+                palmiak_green: "#2BFF88",
+            },
+        },
+    },
+    plugins: [require("@tailwindcss/typography")],
+};
