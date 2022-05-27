83b11f7b78a97ea3cefd13a11817084aa841f03f
Maciej Palmowski <m.palmowski@freshpixels.pl>
2022-05-27 15:35:52 +0200

    WCEU materials

---
+++ database/migrations/2022_05_26_090656_create_events_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('title');
+            $table->string('content')->nullable();
+            $table->string('slug')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
