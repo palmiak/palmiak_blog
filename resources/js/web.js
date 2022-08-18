7ef437f0b1a2e99f98744b438725e25d92c0976b
Maciej Palmowski <m.palmowski@freshpixels.pl>
2022-08-18 22:39:33 +0200

    styling fixes

--- resources/js/web.js
+++ resources/js/web.js
@@ -1,8 +1,11 @@
 import Alpine from 'alpinejs';
-const hljs = require('highlight.js/lib/common');
- 
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import php from 'highlight.js/lib/languages/php';
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('php', php);
+
 window.Alpine = Alpine;
- 
+hljs.highlightAll();
 Alpine.start();
 
-hljs.highlightAll();
