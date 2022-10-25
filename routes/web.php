080dfcf7f61dd17db2cc0361e400df7bcec4e49d
Maciek Palmowski <m.palmowski@freshpixels.pl>
2022-10-25 13:42:09 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 // Route::statamic('example', 'example-view', [
 //    'title' => 'Example'
 // ]);
-if( env('app_kinsta') ) {
+if( env('APP_KINSTA') ) {
     URL::forceScheme('https');
 }
