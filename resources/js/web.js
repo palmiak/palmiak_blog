5d3e59800edad80ff6e3ae4434ac8de029e02d1b
maciekpalmowski <136691573+maciekpalmowski@users.noreply.github.com>
2024-02-14 01:13:21 +0100

    quicklinks

--- resources/js/web.js
+++ resources/js/web.js
@@ -2,6 +2,11 @@ import Alpine from 'alpinejs';
 import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
 import php from 'highlight.js/lib/languages/php';
+import quicklink from "quicklink/dist/quicklink.js";
+window.addEventListener('load', () => {
+    quicklink.listen();
+});
+
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('php', php);
 
