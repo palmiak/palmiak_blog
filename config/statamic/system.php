1ecc005fef0a016eccb5566c75e70e634f2b6ef5
maciekpalmowski <136691573+maciekpalmowski@users.noreply.github.com>
2024-06-05 00:55:55 +0200

    New article Update to new statamic

--- config/statamic/system.php
+++ config/statamic/system.php
@@ -112,4 +112,20 @@ return [
     'ajax_timeout' => '600000',
     'pcre_backtrack_limit' => '-1',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Enable Multi-site
+    |--------------------------------------------------------------------------
+    |
+    | Whether Statamic's multi-site functionality should be enabled. It is
+    | assumed Statamic Pro is also enabled. To get started, you can run
+    | the `php please multisite` command to update your content file
+    | structure, after which you can manage your sites in the CP.
+    |
+    | https://statamic.dev/multi-site
+    |
+    */
+
+    'multisite' => false,
+
 ];
