2707d1b7d8f99ed8dfcb679179e562552e4f0691
maciekpalmowski <136691573+maciekpalmowski@users.noreply.github.com>
2025-04-08 22:18:00 +0200

    Update higlighter

--- resources/js/web.js
+++ resources/js/web.js
@@ -2,6 +2,7 @@ import Alpine from 'alpinejs';
 import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
 import php from 'highlight.js/lib/languages/php';
+import typescript from 'highlight.js/lib/languages/typescript';
 import quicklink from "quicklink/dist/quicklink.js";
 import confetti from 'canvas-confetti';
 
@@ -33,6 +34,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('php', php);
+hljs.registerLanguage('typescript', typescript);
+
 
 window.Alpine = Alpine;
 hljs.highlightAll();
